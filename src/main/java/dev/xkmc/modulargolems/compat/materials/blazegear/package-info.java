d9bf0fa9da9e1bd01b22fdf3c2754083afd783c9
lcy0x1 <arthurlcy0x1@gmail.com>
2023-05-03 23:22:31 -0700

    DELETE COMPATS

--- src/main/java/dev/xkmc/modulargolems/compat/materials/blazegear/package-info.java
+++
@@ -1,8 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-
-package dev.xkmc.modulargolems.compat.materials.blazegear;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
