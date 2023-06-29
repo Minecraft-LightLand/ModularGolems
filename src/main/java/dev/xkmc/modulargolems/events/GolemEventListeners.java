eede79a94987a76de88011e0c1c7aa4f97a0fbed
lcy0x1 <arthurlcy0x1@gmail.com>
2023-06-29 21:54:24 +0800

    format

--- src/main/java/dev/xkmc/modulargolems/events/GolemEventListeners.java
+++ src/main/java/dev/xkmc/modulargolems/events/GolemEventListeners.java
@@ -1,12 +1,16 @@
 package dev.xkmc.modulargolems.events;
+
 import dev.xkmc.modulargolems.events.event.GolemEquipEvent;
 import dev.xkmc.modulargolems.events.event.GolemThrowableEvent;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.ThrownTrident;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ArrowItem;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.TridentItem;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-public class GolemEventListeners  {
+
+public class GolemEventListeners {
 	@SubscribeEvent
 	public static void onEquip(GolemEquipEvent event) {
 		if (event.getStack().getItem() instanceof ArrowItem) {
@@ -18,6 +22,7 @@ public class GolemEventListeners  {
 			}
 		}
 	}
+
 	@SubscribeEvent
 	public static void isThrowable(GolemThrowableEvent event) {
 		if (event.getStack().getItem() instanceof TridentItem) {
