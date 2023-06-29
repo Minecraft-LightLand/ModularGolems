23dbbbed26470044df1ee5f5afbd525adc907c6c
D12epic <1619640542@qq.com>
2023-06-29 21:09:09 +0800

    Merge remote-tracking branch 'origin/1.19' into 1.19

--- entity/common/GolemFlags.java
+++
@@ -1,5 +0,0 @@
-package dev.xkmc.modulargolems.content.entity.common;
-
-public enum GolemFlags {
-	IMMUNITY, FIRE_IMMUNE, FREEZE_IMMUNE, THUNDER_IMMUNE, SEE_THROUGH, RECYCLE, FLOAT, SWIM, MENDING, NO_DESTROY
-}
