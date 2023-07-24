47b1ec64d80d200318b18c644c31d28e3873c23a
lcy0x1 <arthurlcy0x1@gmail.com>
2023-07-24 23:00:54 +0800

    fix particles

--- src/main/java/dev/xkmc/modulargolems/events/GolemDispenserBehaviors.java
+++ src/main/java/dev/xkmc/modulargolems/events/GolemDispenserBehaviors.java
@@ -2,21 +2,27 @@ package dev.xkmc.modulargolems.events;
 
 import dev.xkmc.modulargolems.content.item.golem.GolemHolder;
 import dev.xkmc.modulargolems.init.registrate.GolemItems;
+import net.minecraft.core.BlockSource;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
 import net.minecraft.core.dispenser.DispenseItemBehavior;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.phys.Vec3;
 
 public class GolemDispenserBehaviors {
 
 	public static void registerDispenseBehaviors() {
-		DispenseItemBehavior behavior = (source, stack) -> {
-			if (((GolemHolder<?, ?>) stack.getItem()).summon(
-					stack, source.getLevel(),
-					Vec3.atBottomCenterOf(source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING))),
-					null, null)) {
-				stack.shrink(1);
+		DispenseItemBehavior behavior = new DefaultDispenseItemBehavior() {
+			@Override
+			protected ItemStack execute(BlockSource source, ItemStack stack) {
+				if (((GolemHolder<?, ?>) stack.getItem()).summon(
+						stack, source.getLevel(),
+						Vec3.atBottomCenterOf(source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING))),
+						null, null)) {
+					stack.shrink(1);
+				}
+				return stack;
 			}
-			return stack;
 		};
 		DispenserBlock.registerBehavior(GolemItems.HOLDER_GOLEM.get(), behavior);
 		DispenserBlock.registerBehavior(GolemItems.HOLDER_HUMANOID.get(), behavior);
