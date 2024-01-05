a21e252112fda8c260c6638b02d2577f20338f29
lcy0x1 <arthurlcy0x1@gmail.com>
2024-01-05 15:28:45 +0800

    fix screen

--- src/main/java/dev/xkmc/modulargolems/compat/curio/GolemCuriosListScreen.java
+++ src/main/java/dev/xkmc/modulargolems/compat/curio/GolemCuriosListScreen.java
@@ -1,13 +1,14 @@
 package dev.xkmc.modulargolems.compat.curio;
 
 import dev.xkmc.l2tabs.compat.BaseCuriosListScreen;
+import dev.xkmc.l2tabs.tabs.core.ITabScreen;
 import dev.xkmc.l2tabs.tabs.core.TabManager;
 import dev.xkmc.modulargolems.content.entity.common.AbstractGolemEntity;
 import dev.xkmc.modulargolems.content.menu.registry.EquipmentGroup;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
-public class GolemCuriosListScreen extends BaseCuriosListScreen<GolemCuriosListMenu> {
+public class GolemCuriosListScreen extends BaseCuriosListScreen<GolemCuriosListMenu> implements ITabScreen {
 
 	public GolemCuriosListScreen(GolemCuriosListMenu cont, Inventory plInv, Component title) {
 		super(cont, plInv, title);
@@ -18,7 +19,8 @@ public class GolemCuriosListScreen extends BaseCuriosListScreen<GolemCuriosListM
 		super.init();
 		var compat = CurioCompatRegistry.get();
 		assert compat != null;
-		new TabManager<>(this, new EquipmentGroup((AbstractGolemEntity<?, ?>) menu.curios.entity)).init(this::addRenderableWidget, compat.tab);
+		new TabManager<>(this, new EquipmentGroup((AbstractGolemEntity<?, ?>) menu.curios.entity))
+				.init(this::addRenderableWidget, compat.tab);
 	}
 
 }
