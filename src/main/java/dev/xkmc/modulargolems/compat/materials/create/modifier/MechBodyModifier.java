1d6fa8ff4b91b13332ce043a08851f3346278d6f
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-04-24 09:04:52 +0800

    spit out buckets

--- src/main/java/dev/xkmc/modulargolems/compat/materials/create/modifier/MechBodyModifier.java
+++ src/main/java/dev/xkmc/modulargolems/compat/materials/create/modifier/MechBodyModifier.java
@@ -9,6 +9,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -37,15 +38,21 @@ public class MechBodyModifier extends GolemModifier {
 		if (mobile > 0 && mobileTime < threshold ||
 				force > 0 && forceTime < threshold) {
 			ItemStack fuel = golem.getProjectile(CreateCompatRegistry.DUMMY.asStack());
+			LivingEntity self = golem;
 			if (fuel.isEmpty()) {
 				var captain = golem.getCaptain();
 				if (captain != null) {
+					self = captain;
 					fuel = captain.getProjectile(CreateCompatRegistry.DUMMY.asStack());
 				}
 				if (fuel.isEmpty()) return;
 			}
 			int time = ForgeHooks.getBurnTime(fuel, RecipeType.SMELTING);
 			if (time <= 0) return;
+			ItemStack remain = fuel.getCraftingRemainingItem();
+			if (!remain.isEmpty()) {
+				self.spawnAtLocation(remain);
+			}
 			fuel.shrink(1);
 			if (mobile > 0) golem.addEffect(new MobEffectInstance(CreateCompatRegistry.EFF_MOBILE.get(),
 					mobileTime + time, mobile - 1));
