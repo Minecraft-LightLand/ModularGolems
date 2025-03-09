0a9625c23715c5a4535b2f6c2663d957498317c1
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-03-09 16:35:01 +0800

    dependency fix

--- src/main/java/dev/xkmc/modulargolems/compat/materials/cataclysm/modifiers/RageEffect.java
+++ src/main/java/dev/xkmc/modulargolems/compat/materials/cataclysm/modifiers/RageEffect.java
@@ -1,13 +1,13 @@
 package dev.xkmc.modulargolems.compat.materials.cataclysm.modifiers;
 
-import dev.xkmc.l2complements.content.effect.StackingEffect;
 import dev.xkmc.l2core.base.effects.api.InherentEffect;
+import dev.xkmc.modulargolems.compat.materials.common.StackableEffect;
 import dev.xkmc.modulargolems.init.ModularGolems;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 
-public class RageEffect extends InherentEffect implements StackingEffect<RageEffect> {
+public class RageEffect extends InherentEffect implements StackableEffect<RageEffect> {
 
 	public RageEffect(MobEffectCategory category, int color) {
 		super(category, color);
