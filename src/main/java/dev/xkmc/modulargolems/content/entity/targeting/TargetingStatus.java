5e1aad3e16a40aed978cf47787cb44c57c70aaec
Dr-WeiAL <931311372@qq.com>
2026-01-07 18:19:36 +0800

    杂项修改  - 距离分数修改  - 强制击退半成品修改

--- src/main/java/dev/xkmc/modulargolems/content/entity/targeting/TargetingStatus.java
+++ src/main/java/dev/xkmc/modulargolems/content/entity/targeting/TargetingStatus.java
@@ -15,7 +15,7 @@ public record TargetingStatus(LivingEntity target, TargetingReason reason, int o
 			case PREY -> 150;
 			case PREVIOUS -> 120;
 		};
-		score -= Mth.clamp((Math.sqrt(distSqr()) / 5), 0, 5);
+		score -= Mth.clamp((Math.sqrt(distSqr()) / 2), 0, 10);
 		score -= oldCount() * 10;
 		score -= Mth.clamp(yDiff() / 3, 0, 5);
 		return score;
