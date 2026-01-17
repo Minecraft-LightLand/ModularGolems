34bf60a78020cdcc1b2ee8e3f79b94b88f4930af
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-01-17 18:55:59 +0800

    dungeon attack limit to direct hit

--- src/main/java/dev/xkmc/modulargolems/compat/materials/compositematerial/modifier/DungeonAttackModifier.java
+++ src/main/java/dev/xkmc/modulargolems/compat/materials/compositematerial/modifier/DungeonAttackModifier.java
@@ -1,6 +1,7 @@
 package dev.xkmc.modulargolems.compat.materials.compositematerial.modifier;
 
 import dev.xkmc.l2damagetracker.contents.attack.AttackCache;
+import dev.xkmc.l2damagetracker.init.data.L2DamageTypes;
 import dev.xkmc.modulargolems.content.core.StatFilterType;
 import dev.xkmc.modulargolems.content.entity.common.AbstractGolemEntity;
 import dev.xkmc.modulargolems.content.modifier.base.GolemModifier;
@@ -13,6 +14,10 @@ public class DungeonAttackModifier extends GolemModifier {
 
 	@Override
 	public void finalizeHurtTarget(AttackCache cache, AbstractGolemEntity<?, ?> golem, int value) {
+		var event = cache.getLivingAttackEvent();
+		if (event == null) return;
+		var source = event.getSource();
+		if (!source.is(L2DamageTypes.DIRECT)) return;
 		golem.heal(cache.getDamageDealt() * 0.5f * value);//TODO config
 	}
 
