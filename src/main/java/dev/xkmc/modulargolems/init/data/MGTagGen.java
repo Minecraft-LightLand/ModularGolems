cbb346ea9db30458610c9ee81d099a8cf1cd580e
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-02-19 23:21:34 +0800

    brontes for large golem

--- src/main/java/dev/xkmc/modulargolems/init/data/MGTagGen.java
+++ src/main/java/dev/xkmc/modulargolems/init/data/MGTagGen.java
@@ -125,7 +125,8 @@ public class MGTagGen {
 					.addOptional(ModItems.WITHER_ASSULT_SHOULDER_WEAPON.getId())
 					.addOptional(ModItems.VOID_ASSULT_SHOULDER_WEAPON.getId())
 					.addOptional(ModItems.ASTRAPE.getId())
-					.addOptional(ModItems.CERAUNUS.getId());
+					.addOptional(ModItems.CERAUNUS.getId())
+					.addOptional(ModItems.BRONTES.getId());
 		}
 		if (ModList.get().isLoaded(L2Weaponry.MODID)) {
 			var tag = pvd.addTag(LARGE_GOLEM_WEAPONS);
