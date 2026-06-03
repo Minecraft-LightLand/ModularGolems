ff5115eae4b1360772a108e5e854bfe78a12aa23
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-04 02:53:22 +0800

    dynamic reduction cap method

--- src/main/java/dev/xkmc/modulargolems/content/client/pose/BeaconConnonPose.java
+++ src/main/java/dev/xkmc/modulargolems/content/client/pose/BeaconConnonPose.java
@@ -44,30 +44,6 @@ public record BeaconConnonPose(
 
 	@Override
 	public void render(MetalGolemEntity entity, MetalGolemModel model, ItemStack stack, InteractionHand hand, PoseStack pose, MultiBufferSource source, int light, float pTick) {
-		if (!Minecraft.getInstance().options.renderDebug || entity.animState.getStartingAnim() < 5) return;
-		var vc = source.getBuffer(RenderType.LINES);
-		var angles = CannonPoseUtil.BEACON_CANNON.getAngle(entity, hand);
-		pose.pushPose();
-		pose.translate(0, 1.5f, 0);
-		pose.scale(1, -1, -1);
-		pose.translate(x * (7f / 16f), 33 / 16f, -3f / 16f);
-		pose.mulPose(Axis.YP.rotation(angles[0] + entity.yBodyRot * Mth.DEG_TO_RAD));
-		pose.mulPose(Axis.XP.rotation(angles[1]));
-		pose.translate(0, 4.5f / 16f, 17 / 16f);
-		render(pose, vc, 0, 0, 0, 1);
-		pose.popPose();
 	}
-
-	public static void render(PoseStack pose, VertexConsumer vc, float x0, float y0, float z0, int len) {
-		var mat = pose.last().pose();
-		var normal = pose.last().normal();
-		vc.vertex(mat, x0 - 1, y0, z0).color(0xFFFF0000).normal(normal, 1, 0, 0).endVertex();
-		vc.vertex(mat, x0 + 1, y0, z0).color(0xFFFF0000).normal(normal, 1, 0, 0).endVertex();
-		vc.vertex(mat, x0, y0 - 1, z0).color(0xFF00FF00).normal(normal, 0, 1, 0).endVertex();
-		vc.vertex(mat, x0, y0 + 1, z0).color(0xFF00FF00).normal(normal, 0, 1, 0).endVertex();
-		vc.vertex(mat, x0, y0, z0 - 1).color(0xFF0000FF).normal(normal, 0, 0, 1).endVertex();
-		vc.vertex(mat, x0, y0, z0 + len).color(0xFF0000FF).normal(normal, 0, 0, 1).endVertex();
-	}
-
 
 }
