c2733ad3ef61bcc2b58e54102bcc6f1386d7b41a
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-12 18:20:52 +0800

    mob skin improvement

--- src/main/java/dev/xkmc/modulargolems/content/entity/humanoid/skin/PlayerSkinInputScreen.java
+++ src/main/java/dev/xkmc/modulargolems/content/entity/humanoid/skin/PlayerSkinInputScreen.java
@@ -1,14 +1,15 @@
 package dev.xkmc.modulargolems.content.entity.humanoid.skin;
 
 import dev.xkmc.modulargolems.content.entity.humanoid.HumanoidGolemEntity;
-import dev.xkmc.modulargolems.content.menu.equipment.EquipmentTab;
 import dev.xkmc.modulargolems.content.menu.registry.OpenEquipmentMenuToServer;
 import dev.xkmc.modulargolems.init.ModularGolems;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
+import org.lwjgl.glfw.GLFW;
 
 public class PlayerSkinInputScreen extends Screen {
 
@@ -17,11 +18,13 @@ public class PlayerSkinInputScreen extends Screen {
 	private static final Component CANCEL = Component.translatable(ModularGolems.MODID + ".gui.player_skin.cancel");
 
 	private final HumanoidGolemEntity golem;
+	private final String prev;
 	private EditBox input;
 
 	public PlayerSkinInputScreen(HumanoidGolemEntity golem) {
 		super(TITLE);
 		this.golem = golem;
+		prev = golem.getPlayerSkin();
 	}
 
 	@Override
@@ -37,14 +40,24 @@ public class PlayerSkinInputScreen extends Screen {
 
 		addRenderableWidget(Button.builder(CONFIRM, e -> confirm())
 				.bounds(cx - 120, cy + 10, 116, 20).build());
-		addRenderableWidget(Button.builder(CANCEL, e -> onClose())
+		addRenderableWidget(Button.builder(CANCEL, e -> cancel())
 				.bounds(cx + 4, cy + 10, 116, 20).build());
 	}
 
-	private void confirm() {
+	private void cancel() {
+		ModularGolems.HANDLER.toServer(SetPlayerSkinToServer.of(golem.getId(), prev));
+		golem.setPlayerSkin(prev);
+		onClose();
+	}
+
+	private void apply() {
 		String value = input.getValue().trim().replace('\\', '/');
 		ModularGolems.HANDLER.toServer(SetPlayerSkinToServer.of(golem.getId(), value));
 		golem.setPlayerSkin(value);
+	}
+
+	private void confirm() {
+		apply();
 		onClose();
 	}
 
@@ -59,14 +72,35 @@ public class PlayerSkinInputScreen extends Screen {
 		super.render(g, mx, my, ptick);
 		g.drawCenteredString(font, TITLE, width / 2, height / 2 - 40, 0xFFFFFF);
 		g.drawCenteredString(font, Component.translatable(ModularGolems.MODID + ".gui.player_skin.hint"), width / 2, height / 2 + 40, 0x808080);
+		renderPreview(g, mx, my);
 	}
 
-	public boolean keyPressed(int p_97878_, int p_97879_, int p_97880_) {
-		if (p_97878_ == 256) {
+	private void renderPreview(GuiGraphics g, int mx, int my) {
+		int w = 40;
+		int h = 40;
+
+		int x = width / 2 - 120 - w / 2;
+		int y = height / 2 + h / 2;
+		double lx = x - mx;
+		double ly = y - h - my;
+		int scale = 24;
+		float ax = (float) Math.atan(lx / 50.0);
+		float ay = (float) Math.atan(ly / 50.0);
+		scale = (int) (scale / golem.getScale());
+		InventoryScreen.renderEntityInInventoryFollowsAngle(g, x, y, scale, ax, ay, golem);
+	}
+
+	public boolean keyPressed(int key, int p_97879_, int p_97880_) {
+		if (key == 256) {
 			onClose();
+			return true;
 		}
-		return !this.input.keyPressed(p_97878_, p_97879_, p_97880_) && !this.input.canConsumeInput() ?
-				super.keyPressed(p_97878_, p_97879_, p_97880_) : true;
+		if (key == GLFW.GLFW_KEY_ENTER) {
+			apply();
+			return true;
+		}
+		return !this.input.keyPressed(key, p_97879_, p_97880_) && !this.input.canConsumeInput() ?
+				super.keyPressed(key, p_97879_, p_97880_) : true;
 	}
 
 }
